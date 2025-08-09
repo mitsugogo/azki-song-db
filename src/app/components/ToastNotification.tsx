9cdeb6b39e29ca766e7d3e5c6f8aec05b8cbc4cb
mitsugogo <mitsugogo@gmail.com>
2025-08-09 13:09:50 +0900

    Flowbite導入

--- src/app/components/ToastNotification.tsx
+++ src/app/components/ToastNotification.tsx
@@ -1,6 +1,8 @@
+import { Toast, ToastToggle } from "flowbite-react";
 import { useState, useEffect } from "react";
+import { HiCheck, HiExclamation, HiX } from "react-icons/hi";
 
-const ToastNotification = ({ message, duration = 3000, onClose }) => {
+const ToastNotification = ({ message, toastType = "success", duration = 3000, onClose }) => {
   const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
@@ -13,12 +15,44 @@ const ToastNotification = ({ message, duration = 3000, onClose }) => {
   }, [duration, onClose]);
 
   if (!isVisible) return null;
+  if (toastType === "error") {
+    return (
+      <div className="absolute top-4 right-6">
+        <Toast>
+          <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-red-100 text-red-500 dark:bg-red-800 dark:text-red-200">
+            <HiExclamation className="h-5 w-5" />
+          </div>
+          <div className="ml-3 text-sm font-normal">{message}</div>
+          <ToastToggle />
+        </Toast>
+      </div>
+    )
+  }
+  if (toastType === "warning") {
+    return (
+      <div className="absolute top-4 right-6">
+        <Toast>
+          <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-orange-100 text-orange-500 dark:bg-orange-700 dark:text-orange-200">
+            <HiExclamation className="h-5 w-5" />
+          </div>
+          <div className="ml-3 text-sm font-normal">{message}</div>
+          <ToastToggle />
+        </Toast>
+      </div>
+    )
+  }
 
   return (
-    <div className="fixed top-4 right-4 bg-white text-gray-800 px-4 py-2 rounded-md shadow-lg">
-      {message}
+    <div className="absolute top-4 right-6">
+      <Toast>
+        <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
+          <HiCheck className="h-5 w-5" />
+        </div>
+        <div className="ml-3 text-sm font-normal">{message}</div>
+        <ToastToggle />
+      </Toast>
     </div>
-  );
+  )
 };
 
 export default ToastNotification;
