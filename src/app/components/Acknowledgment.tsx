fdf3ceefb1ab841a95e4148d08aba079e3350a28
mitsugogo <mitsugogo@gmail.com>
2025-08-11 03:18:36 +0900

    change: 謝辞の追加

--- src/app/components/Acknowledgment.tsx
+++ src/app/components/Acknowledgment.tsx
@@ -6,19 +6,29 @@ const Acknowledgment = () => {
     <>
       <div className="text-sm text-muted">
         <p>本サイトは有志による非公式のファンサイトです。</p>
-        <p>AZKiさんの素敵な歌声をもっとみんなに知ってもらうために制作しました。</p>
-        <p>気になった歌や動画はどんどんSNSにシェアして盛り上げてください！</p>
+        <p>ホロライブ所属のAZKiさんの素敵な歌声を、もっと多くの方々に知ってもらうために制作しました。</p>
+        <p>気になった歌や動画はどんどんSNSにシェアして、AZKiさんのYouTubeチャンネルを盛り上げてください！</p>
       </div>
 
-      <div className="text-sm text-muted mt-3">
-        <p>動画はホロライブプロダクション様及びAZKi様が制作したものです。</p>
-        <p>動画の権利は制作者に帰属します。</p>
+      <h3 className="mt-4 font-semibold">動画について</h3>
+
+      <div className="text-sm text-muted mt-1">
+        <p>動画やアーカイブはホロライブプロダクション様及びAZKi様が制作・配信したものです。</p>
+        <p>動画の権利は所有者に帰属します。</p>
       </div>
 
-      <h3 className="mt-4">Copyright</h3>
-      
+      <h3 className="mt-4 font-semibold">集計について</h3>
+
+      <div className="text-sm text-muted mt-1">
+        <p>集計対象は、AZKiさんのYouTubeチャンネルや他のホロメンや他事務所等にゲスト参加されたあずきんち歌枠・記念ライブ・AZKi生放送・Music Video・カバーMVなどの御本人の歌唱動画を対象にしています。</p>
+        <p>集計は手作業で行っているため、更新の遅延や誤り、表記揺れなどがあるかもしれません。</p>
+        <p>情報の不足や誤りに気づいた場合は、<Link href="https://x.com/mitsugogo" target="_blank" rel="noopener noreferrer">@mitsugogo</Link>までご連絡ください。</p>
+      </div>
+
+      <h3 className="mt-4 font-semibold">Copyright</h3>
+
       <div className="text-sm text-muted">
-        <p>© 2025 <Link href="https://x.com/mitsugogo" target="_blank" rel="noopener noreferrer">@mitsugogo</Link></p>
+        <p>© 2025 <Link href="https://x.com/mitsugogo" target="_blank" rel="noopener noreferrer">mitsugogo</Link></p>
       </div>
     </>
   )
