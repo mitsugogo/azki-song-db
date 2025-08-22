7981dcb4166b061df53672a81fea5a82d8e7945e
Mitsuhiro GOTA <mitsugogo@gmail.com>
2025-08-22 15:53:05 +0900

    サムネイルロード中のアニメーション

--- src/app/components/YoutubeThumbnail.tsx
+++ src/app/components/YoutubeThumbnail.tsx
@@ -1,6 +1,8 @@
 // components/YoutubeThumbnail.tsx
 import Image from "next/image";
 import useYoutubeThumbnailFallback from "../hook/useYoutubeThumbnailFallback";
+import { useState } from "react";
+import { Spinner } from "flowbite-react";
 
 interface YoutubeThumbnailProps {
   videoId: string;
@@ -22,30 +24,49 @@ const YoutubeThumbnail: React.FC<YoutubeThumbnailProps> = ({
   outcontainerClassName,
 }) => {
   const { imageUrl, handleError } = useYoutubeThumbnailFallback(videoId);
+  const [loading, setLoading] = useState(true);
 
   if (fill) {
     return (
       <div className={`relative aspect-video ${outcontainerClassName || ""}`}>
+        {loading && (
+          <>
+            <Spinner />
+          </>
+        )}
         <Image
           src={imageUrl}
           alt={alt}
           fill={true}
           className={`outfit-image ${imageClassName || ""}`}
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          onLoadingComplete={() => setLoading(false)}
           onError={handleError}
+          style={{ opacity: loading ? 0 : 1, transition: "opacity 0.5s" }}
         />
       </div>
     );
   }
 
   return (
-    <div style={{ position: "relative", width, height }}>
+    <div className={`relative aspect-video ${outcontainerClassName || ""}`}>
+      {loading && (
+        <>
+          <Spinner />
+        </>
+      )}
       <Image
         src={imageUrl}
         alt={alt}
-        layout="fill"
-        onError={handleError}
-        unoptimized
+        fill={true}
+        className={`outfit-image ${imageClassName || ""}`}
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        onLoadingComplete={() => setLoading(false)}
+        onError={(e) => {
+          setLoading(false);
+          handleError(e);
+        }}
+        style={{ opacity: loading ? 0 : 1, transition: "opacity 0.5s" }}
       />
     </div>
   );
