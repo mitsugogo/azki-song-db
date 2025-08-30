7775b6797b1c168969b46f0dababe4048f887975
Mitsuhiro GOTA <mitsugogo@gmail.com>
2025-08-30 17:59:52 +0900

    style: スマホ向けのレイアウト調整

--- src/app/components/SongListItem.tsx
+++ src/app/components/SongListItem.tsx
@@ -18,7 +18,7 @@ const SongListItem = React.memo(
     return (
       <li
         // 選択状態に応じて Tailwind CSS のクラスを動的に適用します。
-        className={`rounded relative cursor-pointer transition shadow-md ${
+        className={`rounded relative cursor-pointer transition shadow-md flex md:block ${
           isSelected
             ? "bg-primary-300 hover:bg-primary-400 dark:bg-primary-900 dark:hover:bg-primary-800 dark:text-gray-300"
             : "bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700"
@@ -29,9 +29,10 @@ const SongListItem = React.memo(
         data-video-id={song.video_id}
         data-start-time={song.start}
         data-title={song.title}
+        key={`${song.video_id}-${song.start}`}
       >
-        <div className="block w-full mb-2 text-center">
-          <div className="aspect-video">
+        <div className="flex md:block md:w-full mb-0 md:mb-2 text-center">
+          <div className="aspect-video w-26 md:w-full">
             <YoutubeThumbnail
               videoId={song.video_id}
               alt={song.video_title}
@@ -39,11 +40,11 @@ const SongListItem = React.memo(
             />
           </div>
         </div>
-        <div className="w-full p-3 pt-0">
-          <div className="w-full text-sm font-semibold line-clamp-3">
+        <div className="w-full p-0 pl-2 pt-1 md:pl-3 md:p-3 md:pt-0 truncate">
+          <div className="w-full text-sm font-semibold line-clamp-1 md:line-clamp-3 truncate">
             {song.title}
           </div>
-          <div className="w-full text-xs text-muted-foreground line-clamp-3">
+          <div className="w-full text-xs text-muted-foreground line-clamp-1 md:line-clamp-3 truncate">
             {song.artist} - {song.sing}
           </div>
           <div className="flex gap-x-2 text-xs text-gray-600 dark:text-gray-500">
@@ -51,7 +52,7 @@ const SongListItem = React.memo(
               <>{new Date(song.broadcast_at).toLocaleDateString()}</>
             )}
           </div>
-          <div className="flex w-full gap-x-2 mt-1 text-xs truncate">
+          <div className="absolute bottom-0 right-2 md:top-0 md:left-0 text-xs truncate">
             <div>
               <MilestoneBadge song={song} outClassName="mb-1.5" />
             </div>
