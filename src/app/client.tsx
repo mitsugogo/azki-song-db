11758f7fadb8a4e5795eb9ceae7a211c3bb40f38
Mitsuhiro GOTA <mitsugogo@gmail.com>
2025-09-01 10:33:31 +0900

    fix(main): 高解像度モニタでも画面いっぱいに表示するように

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -12,7 +12,7 @@ export default function ClientTop() {
     <ThemeProvider>
       <div className="h-dvh flex flex-col">
         <Header />
-        <main className="flex flex-col md:flex-row flex-grow overflow-hidden 4xl:container 4xl:mx-auto p-0 lg:p-4 mb:pb-0 dark:bg-gray-900">
+        <main className="flex flex-col md:flex-row flex-grow overflow-hidden p-0 lg:p-4 mb:pb-0 dark:bg-gray-900">
           <MainPlayer />
         </main>
         <Footer />
