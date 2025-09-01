9d009a905ccbc8f468096a63f3dd9cfb1fe55b08
Mitsuhiro GOTA <mitsugogo@gmail.com>
2025-09-01 09:57:18 +0900

    refactor: footerの共通化

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="flex-shrink-0 bg-gray-800 text-white py-2 px-4 text-center hidden lg:block">
+      <p className="text-xs">
+        本サイトは有志による非公式のファンサイトです。
+        <span className="hidden lg:inline">
+          動画はホロライブプロダクション様及びAZKi様が制作したものです。
+        </span>
+        動画の権利は制作者に帰属します。
+      </p>
+    </footer>
+  );
+}
--- src/app/statistics/layout.tsx
+++ src/app/statistics/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "flowbite-react";
 import { Header } from "../components/Header";
 import { AnalyticsWrapper } from "../components/AnalyticsWrapper";
+import Footer from "../components/Footer";
 
 // titleタグ
 export const metadata = {
@@ -20,15 +21,7 @@ export default function StatsLayout({
           <div className="flex flex-col flex-grow md:flex-row overflow-y-hidden w-full">
             {children}
           </div>
-          <footer className="flex-shrink-0 bg-gray-800 text-white py-2 px-4 text-center hidden lg:block">
-            <p className="text-xs">
-              本サイトは有志による非公式のファンサイトです。
-              <span className="hidden lg:inline">
-                動画はホロライブプロダクション様及びAZKi様が制作したものです。
-              </span>
-              動画の権利は制作者に帰属します。
-            </p>
-          </footer>
+          <Footer />
         </div>
         <AnalyticsWrapper />
       </ThemeProvider>
