84d60c24712f0d8d196f6cb2369cf8636a547ecd
Mitsuhiro GOTA <mitsugogo@gmail.com>
2026-05-16 14:29:37 +0900

    fix: update outputFileTracingIncludes in next.config and add engines field in package.json

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 const nextConfig: NextConfig = {
   /* config options here */
   distDir: process.env.NEXT_PLW ? ".next-playwright" : ".next",
+  outputFileTracingIncludes: {
+    "/*": ["node_modules/@swc/helpers/**/*"],
+  },
   images: {
     remotePatterns: [
       {
